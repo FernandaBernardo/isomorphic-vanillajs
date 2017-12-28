42ef7fa5ff327c898b5a09e5f7b481415ddf398a
Fernanda Bernardo <fernandabernardo94@gmail.com>
2017-12-28 13:18:26 -0200

    separate css and js files and make isomorphic code

---
+++ script.js
@@ -0,0 +1,59 @@
+class Author {
+    constructor(firstName, lastName, imageSrc) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.imageSrc = imageSrc;
+    }
+    
+    get name() {
+        return this.firstName + " " + this.lastName;
+    }
+    
+    get image() {
+        return this.imageSrc;
+    }
+}
+
+const parser = {
+    parse: function(authors) {
+        return authors.map((author) => {
+            return new Author(author.name.first, author.name.last, author.picture.medium);
+        });
+    }
+};
+
+const url = 'https://randomuser.me/api/?results=10';
+
+const template = (name, image) => `<li><img src="${image}"><span>${name}</span></li>`;
+
+if (typeof module === 'object') {
+    const https = require('https');
+    https.get(url, res => {
+        res.setEncoding("utf8");
+        let body = "";
+        res.on("data", data => {
+            body += data;
+        });
+        res.on("end", () => {
+            body = JSON.parse(body);
+            let authors = parser.parse(body.results);
+            return authors.map(function(author) {
+                console.log(template(author.name, author.image));
+            })
+        });
+    });
+} else {
+    const ul = document.getElementById('authors');
+    
+    fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+        let authors = parser.parse(data.results);
+        return authors.map(function(author) {
+            ul.insertAdjacentHTML('beforeend', template(author.name, author.image));
+        });
+    })
+    .catch(function(error) {
+        console.log(JSON.stringify(error));
+    });
+}
